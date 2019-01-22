467126991c1a8562ac98e46d58beb4e90e9f7558
luis <yongchun1683@gmail.com>
2019-01-22 10:17:10 +0800

    create routing demo app

---
+++ routing-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponents,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
