beacdea692baf853c1028a560bf8a70295625a63
luis <yongchun1683@gmail.com>
2019-01-22 16:41:30 +0800

    finish basic routing

--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 
 @NgModule({
@@ -12,7 +15,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     AppComponent,
     routingComponents,
     NavComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    DepartmentDetailComponent,
+    DepartmentOverviewComponent,
+    DepartmentContactComponent
   ],
   imports: [
     BrowserModule,
--- routing-app/src/app/department-list/department-list.component.ts
+++ routing-app/src/app/department-list/department-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
 
 @Component({
   selector: 'app-department-list',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentListComponent implements OnInit {
 
-  constructor() { }
+  public departments = [
+    {"id": 1, "name": "Production"},
+    {"id": 2, "name": "Marketing"},
+    {"id": 3, "name": "Accounting"},
+    {"id": 4, "name": "Human Resource"},
+    {"id": 5, "name": "Purchasing"},
+  ]
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  public selectedId;
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId = id;
+    });
+  }
+
+  onSelect(department) {
+    this.router.navigate(['/departments', department.id]);
+    // this.router.navigate([department.id], {relativeTo: this.route});
+
+  }
+
+  isSelected(department) {
+    return department.id === this.selectedId;
   }
 
 }
