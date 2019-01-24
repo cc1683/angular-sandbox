11381d3b30e5997b4404aff588c11fb7d8a4b59f
luis <yongchun1683@gmail.com>
2019-01-24 14:29:33 +0800

    finish tutorial on todoapp without routing

--- angular-todoapp/src/app/components/todo-item/todo-item.component.ts
+++ angular-todoapp/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+  }
+
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    });
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
   }
 
 }
--- angular-todoapp/src/app/components/todos/todos.component.ts
+++ angular-todoapp/src/app/components/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -9,14 +11,23 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos: Todo[];
 
-  constructor() { }
-
+  constructor(private todoService: TodoService) { }  
+ 
   ngOnInit() {
-    this.todos = [
-      {id: 1, title: 'Todo 1', completed: true},
-      {id: 2, title: 'Todo 2', completed: false},
-      {id: 3, title: 'Todo 3', completed: true},
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+       this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe;
+  }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
   }
 
 }
